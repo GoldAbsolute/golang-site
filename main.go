5b96b99b8664c0fb57a5188149ee9c7523605770
GoldAbsolute <sokoldima1998@mail.ru>
2022-08-21 18:31:25 +0300

    test2

--- main.go
+++ main.go
@@ -2,25 +2,42 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	// port env
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
 	if port == "" {
 		log.Fatal("$POST must be set")
 	}
+	// main router
+	MainRouter := mux.NewRouter()
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, err := fmt.Fprintln(writer, "Heh")
-		if err != nil {
-			panic(err)
+	// static files start
+
+	MainRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// static files end
+
+	// routes
+	MainRouter.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/index.tmpl.html", "templates/header.tmpl.html", "templates/nav.tmpl.html"))
+		errT := tmpl.ExecuteTemplate(writer, "index.tmpl.html", nil)
+		if errT != nil {
+			panic(errT)
 		}
 	})
-	err := http.ListenAndServe(":"+port, nil)
+
+	err := http.ListenAndServe(":"+port, MainRouter)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Server has been started")
 }
